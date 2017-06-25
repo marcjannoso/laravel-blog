c9bf2701c37d4d5df67d2d830a24ca17b0843789
marcjannoso <janno_87ph@yahoo.com>
2017-06-25 04:48:29 -0400

    blog create and show is complete

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@section('content')
+
+<main class="container-fluid">
+	<div class="container-fluid">
+		<div class="jumbotron">
+			<h1>Latest Blog Posts</h1>
+		</div>
+	<div class="col-sm-8 col-sm-offset-2">
+       @foreach ($blogs as $blog)
+		   <article>
+		   <h2><a href="{{ action('BlogController@show', [$blog->id]) }}"> 
+		   {{ $blog->title }} </a></h2>
+		   <p>{{ $blog->body }}</p>
+		   </article>
+		@endforeach
+	</div>
+	</div>
+</main>
+
+
+@endsection
